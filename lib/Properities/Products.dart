30e2d3ee9b2469afee3ac2bfa8b0b35f9f67e6ad
Mariam Shaarawy <48493968+mariam1592000@users.noreply.github.com>
2021-08-25 01:39:27 +0200

    Update Products.dart

--- lib/Properities/Products.dart
+++ lib/Properities/Products.dart
@@ -2,31 +2,31 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Products {
-  @required
-  static int id=0;
+  static int counter=0;
+  int productID=0;
   final AssetImage productImage;
-  @required
   final String productName;
+  final String productDescription;
   final double productPrice;
 
-  Products( this.productImage,  this.productName,  this.productPrice)
+  Products( this.productImage,  this.productName, this.productDescription, this.productPrice)
   {
-     id++;
+     this.productID=counter++;
   }
 }
 List<Products> products = [
-  Products( AssetImage("images/Basic coffee.jpg"), 'Basic coffee', 12),
-  Products( AssetImage("images/Basic donuts.jpg"), 'Basic Donuts', 13),
-  Products( AssetImage("images/cappuccino.jpg"),  'cappuccino',  17.5),
-  Products( AssetImage("images/Chocolate donuts.jpg"),  'Chocolate Donuts',  15),
-  Products( AssetImage("images/cookies.jpg"),  'Cookies',  12),
-  Products( AssetImage("images/croissant.jpg"), 'Croissant', 13),
-  Products( AssetImage("images/Espresso.jpg"),  'Espresso',  17.5),
-  Products( AssetImage("images/hot chocolate.jpg"),  'Hot Chocolate',  15),
-  Products( AssetImage("images/Icecream.jpg"),  'Ice Cream',  12),
-  Products( AssetImage("images/iced coffee.jpg"), 'Iced Coffee', 13),
-  Products( AssetImage("images/latte.jpg"),  'Latte',  17.5),
-  Products( AssetImage("images/Peach tart.jpg"),  'Peach Tart',  15),
-  Products( AssetImage("images/Strawberry pancakes.jpg"),  'Strawberry Pancakes',  15),
-
+  Products( AssetImage("images/Basic coffee.jpg"), 'Basic coffee','Basic coffee', 12),
+  Products( AssetImage("images/Basic donuts.jpg"), 'Basic Donuts','Basic Donuts', 13),
+  Products( AssetImage("images/cappuccino.jpg"),  'cappuccino','Basic Donuts',  17.5),
+  Products( AssetImage("images/Chocolate donuts.jpg"), 'Chocolate Donuts','Chocolate Donuts',  15),
+  Products( AssetImage("images/cookies.jpg"),  'Cookies','Chocolate Donuts',  12),
+  Products( AssetImage("images/croissant.jpg"), 'Croissant','Chocolate Donuts', 13),
+  Products( AssetImage("images/Espresso.jpg"),  'Espresso','Chocolate Donuts',  17.5),
+  Products( AssetImage("images/hot chocolate.jpg"),  'Hot Chocolate','Chocolate Donuts',  15),
+  Products( AssetImage("images/Icecream.jpg"),  'Ice Cream','Chocolate Donuts',  12),
+  Products( AssetImage("images/iced coffee.jpg"), 'Iced Coffee','Chocolate Donuts', 13),
+  Products( AssetImage("images/latte.jpg"),  'Latte','Chocolate Donuts',  17.5),
+  Products( AssetImage("images/Peach tart.jpg"),  'Peach Tart','Peach Tart',  15),
+  Products( AssetImage("images/Strawberry pancakes.jpg"),'Peach Tart',  'Strawberry Pancakes',  15)
 ];
+
