750ee08c243535d580a76893a6b2a2572fcbe86e
Mariam Shaarawy <48493968+mariam1592000@users.noreply.github.com>
2021-08-26 17:24:43 +0200

    Update Products.dart

--- lib/Properities/Products.dart
+++ lib/Properities/Products.dart
@@ -15,18 +15,17 @@ class Products {
   }
 }
 List<Products> products = [
-  Products( AssetImage("images/Basic coffee.jpg"), 'Basic coffee','Basic coffee', 12),
-  Products( AssetImage("images/Basic donuts.jpg"), 'Basic Donuts','Basic Donuts', 13),
-  Products( AssetImage("images/cappuccino.jpg"),  'cappuccino','Basic Donuts',  17.5),
-  Products( AssetImage("images/Chocolate donuts.jpg"), 'Chocolate Donuts','Chocolate Donuts',  15),
-  Products( AssetImage("images/cookies.jpg"),  'Cookies','Chocolate Donuts',  12),
-  Products( AssetImage("images/croissant.jpg"), 'Croissant','Chocolate Donuts', 13),
-  Products( AssetImage("images/Espresso.jpg"),  'Espresso','Chocolate Donuts',  17.5),
-  Products( AssetImage("images/hot chocolate.jpg"),  'Hot Chocolate','Chocolate Donuts',  15),
-  Products( AssetImage("images/Icecream.jpg"),  'Ice Cream','Chocolate Donuts',  12),
-  Products( AssetImage("images/iced coffee.jpg"), 'Iced Coffee','Chocolate Donuts', 13),
-  Products( AssetImage("images/latte.jpg"),  'Latte','Chocolate Donuts',  17.5),
-  Products( AssetImage("images/Peach tart.jpg"),  'Peach Tart','Peach Tart',  15),
-  Products( AssetImage("images/Strawberry pancakes.jpg"),'Peach Tart',  'Strawberry Pancakes',  15)
+  Products( AssetImage("images/Basic coffee.jpg"), 'Basic coffee','Basic coffee: Coffee + Hot water + Desired Sugar', 12),
+  Products( AssetImage("images/cappuccino.jpg"),  'Cappuccino','Cappuccino: Coffee + Milk + Sugar + vanilla Extract',  17.5),
+  Products( AssetImage("images/Basic donuts.jpg"), 'Basic Donuts','Basic Donuts: Flour + Baking Powder + Salt + Buttermilk + Granulated sugar + Melted Butter + Vegetable Oil + Vanilla Extract', 13),
+  Products( AssetImage("images/Chocolate donuts.jpg"), 'Chocolate Donuts','Chocolate Donuts: Flour + Baking Powder + Salt + Buttermilk + Granulated sugar + Melted Butter + Vegetable Oil + Chocolate',  15),
+  Products( AssetImage("images/cookies.jpg"),  'Cookies','Cookies: Flour + Baking Powder + Baking Soda + Cinnamon + Ginger + Nutmeg + Cloves + Salt + Sugar + Packed Light-Brown Sugar + Vegetable Oil + Egg + Vanilla Extract + Canned Pumpkin Puree + Semi-Sweet Chocolate Chips',  12),
+  Products( AssetImage("images/Espresso.jpg"),  'Espresso','Espresso: Coffee + Hot Water + Sugar',  17.5),
+  Products( AssetImage("images/hot chocolate.jpg"),  'Hot Chocolate','Hot Chocolate: Chocolate PoWder + Milk + Sugar',  15),
+  Products( AssetImage("images/Icecream.jpg"),  'Ice Cream','Ice Cream: Heavy Cream + Milk + Granulated Sugar + Salt + Egg Yolks + Desired Taste',  12),
+  Products( AssetImage("images/iced coffee.jpg"), 'Iced Coffee','Iced Coffee: Coffee + Cold Milk + Sugar + Ice Cubes + vanilla Extract', 13),
+  Products( AssetImage("images/latte.jpg"),  'Latte','Latte: Coffee + Milk + Sugar',  17.5),
+  Products( AssetImage("images/Peach tart.jpg"),  'Peach Tart','Peach Tart: Peaches + Fresh Lemon Juice + Granulated Sugar + Cornstarch + Cinnamon + Nutmeg',  15),
+  Products( AssetImage("images/Strawberry pancakes.jpg"),'Strawberry Pancakes',  'Strawberry Pancakes: Flour + Granulated Sugar + Baking Powder + Baking Soda + Salt + Milk + Sour Cream + Eggs + Melted Unsalted Butter + Cold Butter',  15)
 ];
 
